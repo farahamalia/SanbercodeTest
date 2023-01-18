7b9e1da0c68c2b7f28a0e8ec8e090fc3ecfaa14a
farahamalia <farahlia.amalia@gmail.com>
2023-01-17 16:36:32 -0800

    Update Tes_Sanbercode.py

--- Tes_Sanbercode.py
+++ Tes_Sanbercode.py
@@ -11,26 +11,25 @@ class TestLogin(unittest.TestCase):
 
     def setUp(self): 
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
- 
-    def test_login_negative_empty_email_and_password(self): 
+    
+    def test_01login_positive(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
-        browser.find_element(By.ID, "user-name").send_keys("") # isi username
+        browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
         time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("") # isi password
+        browser.find_element(By.ID, "password").send_keys("secret_sauce") # isi password
         time.sleep(1)
         browser.find_element(By.ID, "login-button").click() # klik tombol sign in
-        time.sleep(1)
-        
-        response_message = browser.find_element(By.CLASS_NAME,"error-button").text
-        self.assertEqual(response_message, 'Epic sadface: Username is required')
-        
-    def test_login_negative_wrong_password(self): 
+        time.sleep(1) 
+    
+    def test_02login_negative_wrong_password(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
         browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
         time.sleep(1)
@@ -39,25 +38,30 @@ class TestLogin(unittest.TestCase):
         browser.find_element(By.ID, "login-button").click() # klik tombol sign in
         time.sleep(1)
         
-        response_message = browser.find_element(By.CSS_SELECTOR,"error-message-container error").text
-        self.assertEqual(response_message, 'Epic sadface: Username and password do not match any user in this service')
-        
-    def test_login_positive(self): 
+        response_message = browser.find_element(By.CLASS_NAME,"error-message-container").text
+        self.assertEqual(response_message, 'Epic sadface: Username and password do not match any user in this service')    
+   
+    def test_03login_negative_empty_email_and_password(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
-        browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
+        browser.find_element(By.ID, "user-name").send_keys("") # isi username
         time.sleep(1)
-        browser.find_element(By.ID, "password").send_keys("secret_sauce") # isi password
+        browser.find_element(By.ID, "password").send_keys("") # isi password
         time.sleep(1)
         browser.find_element(By.ID, "login-button").click() # klik tombol sign in
-        time.sleep(1) 
-      
-    def test_add_and_remove_product(self): 
+        time.sleep(1)
+        
+        response_message = browser.find_element(By.CLASS_NAME,"error-message-container").text
+        self.assertEqual(response_message, 'Epic sadface: Username is required')
+    
+    def test_04add_and_remove_product(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
         browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
         time.sleep(1)
@@ -68,12 +72,13 @@ class TestLogin(unittest.TestCase):
         browser.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click() # pilih product
         time.sleep(1) 
         browser.find_element(By.ID, "remove-sauce-labs-backpack").click() # remove product
-        time.sleep(1)  
-        
-    def test_checkOut_negative_empty_field(self): 
+        time.sleep(1) 
+    
+    def test_05checkOut_negative_empty_field(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
         browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
         time.sleep(1)
@@ -97,13 +102,14 @@ class TestLogin(unittest.TestCase):
         time.sleep(1)
 
         # validasi
-        response_message = browser.find_element(By.CSS_SELECTOR,"error").text
-        self.assertEqual(response_message, 'Error: First Name is required')    
-        
-    def test_checkOut_positive(self): 
+        response_message = browser.find_element(By.CLASS_NAME,"error-message-container").text
+        self.assertEqual(response_message, 'Error: First Name is required')   
+      
+    def test_06checkOut_positive(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://www.saucedemo.com") # buka situs
+        browser.maximize_window()
         time.sleep(3)
         browser.find_element(By.ID, "user-name").send_keys("standard_user") # isi username
         time.sleep(1)
@@ -129,13 +135,12 @@ class TestLogin(unittest.TestCase):
         time.sleep(1)
 
         # validasi
-        response_data = browser.find_element(By.CSS_SELECTOR,"complete-header").text
-        response_message = browser.find_element(By.CSS_SELECTOR,"complete-text").text
+        response_message = browser.find_element(By.CLASS_NAME,"complete-text").text
         
-        self.assertIn('THANK YOU FOR YOUR ORDER', response_data)
-        self.assertEqual(response_message, 'Your order has been dispatched, and will arrive just as fast as the pony can get there!')
-        
-def tearDown(self): 
+        self.assertEqual(response_message, 'Your order has been dispatched, and will arrive just as fast as the pony can get there!')     
+  
+    
+    def tearDown(self): 
         self.browser.close() 
 
 if __name__ == "__main__": 
