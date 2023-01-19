9c475be34d0884034b2d25f108dfe24c6bc09b38
farahamalia <farahlia.amalia@gmail.com>
2023-01-18 23:37:33 -0800

    Update TugasDay_18_Sanbercode.cy.js

--- TugasDay_18_Sanbercode.cy.js
+++ TugasDay_18_Sanbercode.cy.js
@@ -1,32 +1,37 @@
 /// <reference types="Cypress" />
 
-describe("My First Test", () => {
+/// <reference types='Cypress' />
+
+describe('My First Test', () => {
 
   it('login is success', () => {
-    cy.visit("https://the-internet.herokuapp.com/login")
+    cy.visit('https://the-internet.herokuapp.com/login')
     // Fill the username
-    cy.get("#username").type("tomsmith")  
-    .should("have.value","tomsmith")
+    cy.get('#username')
+    .type('tomsmith')  
+    .should('have.value','tomsmith')
+    cy.wait(3)
     // Fill the password
-    cy.get("#password").type("SuperSecretPassword!")
-    .should("have.value","SuperSecretPassword!")
-    // Locate and submit the form
-    cy.get(".fa").click()
-    
+    cy.get('#password')
+    .type('SuperSecretPassword!')
+    .should('have.value','SuperSecretPassword!')
+    //submit the form
+    cy.get('#login').click()
+
   })
-  it("login is fail", () =>{
-    cy.visit("https://the-internet.herokuapp.com/login")
+  it('login is fail', () =>{
+    cy.visit('https://the-internet.herokuapp.com/login')
     // Fill the username
-    cy.get("#username").type("tomsmith")  
-    .should("have.value","tomsmith")
+    cy.get('#username').type('tomsmith')  
+    .should('have.value','tomsmith')
+    cy.wait(3)
     // Fill the password
-    cy.get("#password").type("salah")
-    //.should("have.value","SuperSecretPassword!")
-    // Locate and submit the form
-    cy.get(".fa").click();
-    cy.get('#flash')
-  }
-  )
+    cy.get('#password').type('salah')
+    //submit the form
+    cy.get('#login').click()
+    cy.get('#flash-messages')
+ }
+)
 
   
   
