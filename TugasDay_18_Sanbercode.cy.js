38470fd2978038fe69de9f9a4c42266c386758ea
farahamalia <farahlia.amalia@gmail.com>
2023-01-18 18:42:06 -0800

    Update and rename TugasDay_18_Sanbercode to TugasDay_18_Sanbercode.cy.js

---
+++ TugasDay_18_Sanbercode.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+describe("My First Test", () => {
+
+  it('should fill login form and redirect to homepage', () => {
+    cy.visit("https://the-internet.herokuapp.com/login")
+    // Fill the username
+    cy.get("#username").type("tomsmith")  
+    .should("have.value","tomsmith")
+    // Fill the password
+    cy.get("#password").type("SuperSecretPassword!")
+    .should("have.value","SuperSecretPassword!")
+    // Locate and submit the form
+    cy.get(".fa").click()
+    
+  })
+  it("login is fail", () =>{
+    cy.visit("https://the-internet.herokuapp.com/login")
+    // Fill the username
+    cy.get("#username").type("tomsmith")  
+    .should("have.value","tomsmith")
+    // Fill the password
+    cy.get("#password").type("salah")
+    //.should("have.value","SuperSecretPassword!")
+    // Locate and submit the form
+    cy.get(".fa").click();
+    cy.get('#flash')
+  }
+  )
+
+  
+  
+});
